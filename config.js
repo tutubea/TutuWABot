163aa1b44dd25d36d67ba3e8f95a6bebf76b4861
tutubea <166099278+tutubea@users.noreply.github.com>
2024-08-10 22:07:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/700e384675867155515b0.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hi I am Tutu MD, I am alive now!",
 };
